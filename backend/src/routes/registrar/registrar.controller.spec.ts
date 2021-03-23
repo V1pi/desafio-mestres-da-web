4c8ad5acbe4b7af2a2406ab3cb37010d4e4bdf0a
v1pi <vimivini99@gmail.com>
2021-03-23 13:56:27 -0300

    Feito testes das rotas novas

--- backend/src/routes/registrar/registrar.controller.spec.ts
+++ backend/src/routes/registrar/registrar.controller.spec.ts
@@ -30,4 +30,36 @@ describe('RegistrarController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined()
   })
+
+  it('should create administrador', async () => {
+    const shouldReturn = {
+      error_id: -1,
+      message: 'Sucesso!',
+      error: false,
+      data: {
+        administrador: {
+          nome: 'Vinicius Picanço',
+          email: 'vimivini99@gmail.com',
+          papel: 0,
+          uid: 'm4ug9Dz5CzSTtMD28Z8nhNjqWWH2',
+          id: 1,
+        },
+      },
+    }
+
+    const dto = {
+      nome: 'Vinicius Picanço',
+      email: 'vimivini99@gmail.com',
+      senha: '123456',
+    } as any
+
+    repo.create.mockResolvedValue(shouldReturn.data.administrador as any)
+    mockFirebaseUser.createUser.mockResolvedValue({
+      uid: 'm4ug9Dz5CzSTtMD28Z8nhNjqWWH2',
+    } as any)
+    mockFirebaseUser.setCustomUserClaims.mockReturnThis()
+    await expect(controller.registerCliente(dto)).resolves.toStrictEqual(
+      shouldReturn,
+    )
+  })
 })
