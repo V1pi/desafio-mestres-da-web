457137fe9fc8acca1a09fa384be26de3df79f8bf
v1pi <vimivini99@gmail.com>
2021-03-25 00:09:39 -0300

    Ativado hooks

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { AppConfigService } from './config/app/config.service'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true })
   const appConfig: AppConfigService = app.get('AppConfigService')
+  app.enableShutdownHooks()
   await app.listen(appConfig.port, appConfig.host)
 }
 bootstrap()
