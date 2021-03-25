2de4dce0c41a1673ab7f19073dc6350eed61dec2
v1pi <vimivini99@gmail.com>
2021-03-25 00:01:45 -0300

    Ativado cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module'
 import { AppConfigService } from './config/app/config.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, { cors: true })
   const appConfig: AppConfigService = app.get('AppConfigService')
   await app.listen(appConfig.port, appConfig.host)
 }
