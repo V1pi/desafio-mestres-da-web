f6868980adedb3420123bf19053b251f0eb46c8f
v1pi <vimivini99@gmail.com>
2021-03-28 12:35:31 -0300

    Deletado arquivos que não serão mais usados

--- backend/src/config/app/configuration.ts
+++
@@ -1,6 +0,0 @@
-import { registerAs } from '@nestjs/config'
-export default registerAs('app', () => ({
-  host: process.env.HOST,
-  port: process.env.PORT,
-  env: process.env.NODE_ENV,
-}))
--- backend/src/models/basis/basis.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common'
-import { BaseModel } from './base.entity'
-
-@Module({
-  exports: [BaseModel],
-})
-export class BaseModelModule {}
